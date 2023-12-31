df4720f4318d71a69e475013043611a0cc99c837
AmirKoobani <66202558+AmirKoobani@users.noreply.github.com>
2023-12-31 18:12:12 +0200

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/Watch-Online/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
