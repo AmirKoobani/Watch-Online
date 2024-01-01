fa3ef61694afa1436232ce769865136e117bf413
AmirKoobani <66202558+AmirKoobani@users.noreply.github.com>
2024-01-01 15:22:35 +0200

    feat: finish components splitting

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import ChannelsTabs from './components/ChannelsTabs.vue'
 import VideoPlayer from './components/VideoPlayer/VideoPlayer.vue'
 
-const handleSelectedChannel = (selectedChannelTab: ChannelTab) => {
-  // Update the currentChannelTab in the parent component
-  currentChannelTab.value = selectedChannelTab
+const currentChannel = ref<Channel>({
+  title: 'ערוץ 11',
+  link: 'https://kan11w.media.kan.org.il/hls/live/2105694/2105694/source1_4k/chunklist.m3u8'
+})
+
+const handleChannelChange = (selectedChannel: Channel) => {
+  currentChannel.value = selectedChannel
 }
 </script>
 
 <template>
   <v-app>
     <v-app-bar color="blue" app>
-      <ChannelsTabs @selected-channel="handleSelectedChannel" />
+      <ChannelsTabs @channel-change="handleChannelChange" />
     </v-app-bar>
     <v-main>
       <v-container>
-        <VideoPlayer :video-link="currentChannelTab.link" />
+        <VideoPlayer :video-link="currentChannel.link" />
       </v-container>
     </v-main>
   </v-app>
